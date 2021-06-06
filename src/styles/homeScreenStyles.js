7b43ad0b58298396df76d488bf94cb4cf5aaf453
Truong Luong <ttluong27@gmail.com>
2021-06-06 09:23:45 -0700

    Create fresh expo app.

---
+++ src/styles/homeScreenStyles.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  headerText: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 20,
+  },
+})
+
+export {styles}
